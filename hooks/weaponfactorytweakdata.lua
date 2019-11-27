1943438f866b7cb9b52a5d47add9a7b70af112ea
GermanTacos <jakebuilds@gmail.com>
2019-11-27 03:29:58 -0500

    Create weaponfactorytweakdata.lua

---
+++ hooks/weaponfactorytweakdata.lua
@@ -0,0 +1,8 @@
+Hooks:PostHook( WeaponFactoryTweakData, "init", "AK101Init", function(self)
+	
+--=========================--
+--          AK-101         --
+-- GERMANTACOS, KILLERWOLF --
+--=========================--
+	
+end )
